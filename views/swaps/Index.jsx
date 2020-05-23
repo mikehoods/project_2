6a1047897c778dc13f8cbd02c674a389b6a2f2b5
Michael Hood <michaelhood@Michaels-MacBook-Pro.local>
2020-05-23 14:13:24 -0500

    setup filters to display currentusers swaps on /swaps

--- views/swaps/Index.jsx
+++ views/swaps/Index.jsx
@@ -3,11 +3,30 @@ const Layout = require('../components/Layout.jsx')
 
 class SwapIndex extends React.Component {
     render() {
+        const allSwaps = []
+        {this.props.swaps.map((swap, i) => {
+            return (
+                allSwaps.push(swap)
+            )
+        })}
+        const youRequested = (owner, i) => {
+            
+            return allSwaps[i].requestFrom === this.props.username
+        }
+        const theyRequested = (owner, i) => {
+            
+            return allSwaps[i].owner === this.props.username
+        }
+        const requestedSwaps = (allSwaps.filter(youRequested))
+        const ownedSwaps = (allSwaps.filter(theyRequested))
+
+        console.log(allSwaps)
+        console.log(requestedSwaps)
         return (
             <Layout>
-                <h1>All the Swaps</h1>
+                <h1>{this.props.username}'s Swaps</h1>
                 <div>
-                    {this.props.swaps.map((swap, i) => {
+                    {/* {this.props.swaps.map((swap, i) => {
                         return (
                             <div key={i}>
                                 <h2>{swap.owner}: {swap.title}</h2>
@@ -15,7 +34,27 @@ class SwapIndex extends React.Component {
                                 <p>{swap.msg}</p>
                             </div>
                         )
-                    })}    
+                    })}     */}
+                    <h1>Your Requests</h1>
+                    {requestedSwaps.map((swap, i) => {
+                        return (
+                            <div key={i}>
+                                <h2>{swap.title} - sent to {swap.owner}</h2>
+                                {/* <h3>From: {swap.requestFrom}</h3> */}
+                                <p>{swap.msg}</p>
+                            </div>
+                        )
+                    })}
+                    <h1>Their Requests</h1>
+                    {ownedSwaps.map((swap, i) => {
+                        return (
+                            <div key={i}>
+                                <h2>{swap.title}</h2>
+                                <h3>From: {swap.requestFrom}</h3>
+                                <p>{swap.msg}</p>
+                            </div>
+                        )
+                    })}
                 </div>
                 <a href="/plants" class="btn">Back To Plants</a>
             </Layout>
